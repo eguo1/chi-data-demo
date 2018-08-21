acb4183d733ba864e44455cd6fcd4b6e450d6900
Eric Guo <ericxguo@gmail.com>
2018-08-21 12:31:05 -0500

    removed db.sync force: true

--- server/index.js
+++ server/index.js
@@ -150,7 +150,7 @@ const startListening = () => {
   require('./socket')(io)
 }
 
-const syncDb = () => db.sync({ force: true })
+const syncDb = () => db.sync()
 
 async function bootApp() {
   await sessionStore.sync()
