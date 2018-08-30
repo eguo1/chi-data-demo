75d7e408dbaace8ac30c9671f29c263efa7919cd
Eric Guo <ericxguo@gmail.com>
2018-08-30 14:00:45 -0500

    implemented aggregateCount on ward models

--- server/db/models/ward.js
+++ server/db/models/ward.js
@@ -10,9 +10,36 @@ const Ward = db.define('ward', {
   centroid: {
     type: Sequelize.GEOMETRY('POINT')
   },
-  geoShape: {
+  geom: {
     type: Sequelize.GEOMETRY('MULTIPOLYGON')
   }
 })
+
+const aggregateQuery =
+  `SELECT
+  crime_data."ID",
+  crime_data."Date",
+  crime_data."Block",
+  crime_data."Primary Type",
+  crime_data."Arrest",
+  crime_data."Location"
+  FROM wards, crime_data
+  WHERE ST_Contains(wards.geom, crime_data."Location")
+  AND wards.name = :wardName;`
+
+Ward.prototype.aggregateList = function () {
+  return db.query(
+    aggregateQuery,
+    { replacements: { wardName: this.name }, type: Sequelize.QueryTypes.SELECT }
+  )
+}
+
+Ward.prototype.aggregateCount = async function () {
+  const allPoints = await db.query(
+    aggregateQuery,
+    { replacements: { wardName: this.name }, type: Sequelize.QueryTypes.SELECT }
+  )
+  return allPoints.length
+}
 
 module.exports.Ward = Ward
