a126e3bf1b5977e80e57bb1a4edcc1587a29d225
Eric Guo <ericxguo@gmail.com>
2018-09-03 15:52:35 -0500

    added actions, thunks, reducer for getting ward data

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,11 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import wards from './ward'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user, wards
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
---
+++ client/store/ward.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import axios from 'axios'
+
+const GET_WARDS = 'GET_WARDS'
+
+const defaultWards = []
+
+const getWards = wards => ({ type: GET_WARDS, wards })
+
+export const getWardData = () => async dispatch => {
+  try {
+    const { data } = await axios.get('/api/wards/')
+    dispatch(getWards(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export default function(initialState = defaultWards, action) {
+  switch (action.type) {
+    case GET_WARDS:
+      return [ ...action.wards ]
+    default:
+      return initialState
+  }
+}
